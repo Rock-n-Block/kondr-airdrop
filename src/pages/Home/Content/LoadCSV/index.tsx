a5fa9eb6c34badaa7512d4d9be4b87e971a18a23
Andrey <mastercatpro.tm@gmail.com>
2022-02-01 16:00:37 +0300

    add autogenerator

--- src/pages/Home/Content/LoadCSV/index.tsx
+++ src/pages/Home/Content/LoadCSV/index.tsx
@@ -17,6 +17,12 @@ const calcTotal = (data: CSVLine[]) => {
   return data.map((line) => +line.amount).reduce((acc, val) => acc + val, 0);
 };
 
+function getRandomIntInclusive(min: number, max: number) {
+  const minimal = Math.ceil(min);
+  const maximal = Math.floor(max);
+  return Math.floor(Math.random() * (maximal - minimal + 1)) + minimal;
+}
+
 const LoadCSV: VFC = () => {
   const dispatch = useTypedDispatch();
   const { setFiles, setFile, setIsLoading, setError } = FileSlice.actions;
@@ -41,9 +47,12 @@ const LoadCSV: VFC = () => {
           const CSVData: CSVLine[] = [];
           lines.forEach((line, key) => {
             if (line.length > 0) {
-              const [address, amount, data] = line.replaceAll('"', '').split(',');
+              const [address, amount, data] = line
+                .replaceAll('"', '')
+                .replaceAll(';', ',')
+                .split(',');
               if (!web3utils.checkAddressChecksum(address)) {
-                err.push(`error address at line ${key + 1}`);
+                err.push(`error address at line ${key + 1}\n${address}`);
                 return;
               }
               if (
@@ -51,11 +60,11 @@ const LoadCSV: VFC = () => {
                 new Date(parseInt(data, 10) * 1000).getTime() < Date.now() ||
                 Number.isNaN(new Date(parseInt(data, 10) * 1000))
               ) {
-                err.push(`error date at line ${key + 1}`);
+                err.push(`error date at line ${key + 1}\n${data}`);
                 return;
               }
               if (Number.isNaN(parseFloat(amount))) {
-                err.push(`error amount at line ${key + 1}`);
+                err.push(`error amount at line ${key + 1}\n${amount}`);
                 return;
               }
               const idx = CSVData.findIndex(
@@ -82,7 +91,7 @@ const LoadCSV: VFC = () => {
           logger('errors', err);
           logger(
             'table',
-            CSVData.map((val) => `${val.address} ${new Date(+val.data*1000)} ${val.amount}\n`),
+            CSVData.map((val) => `${val.address} ${new Date(+val.data * 1000)} ${val.amount}\n`),
           );
           if (CSVData.length > 0) {
             dispatch(setState(3));
@@ -172,9 +181,25 @@ const LoadCSV: VFC = () => {
   }, [ReadFile]);
 
   const onSample = useCallback(async () => {
-    const csvExampleText = await fetch('/src/assets/files/sample.csv')
-      .then((res) => res.text())
-      .then((text) => text);
+    const addrs = [
+      '0x063C22e0917b4B051cb81Ef91c5052Cd8C0D0E56',
+      '0xBF7E42e9254A5E64D946bD206120ae5BafaC7781',
+      '0x4d9806EB6FF37a8ab8375058CB43556Aa5763Db0',
+    ];
+    const repeats = 2;
+    const delay = 20 * 60;
+    const timestamp = Date.now() / 1000 + delay;
+    const csvData: any[] = [];
+    for (let i = 0; i < repeats; i += 1) {
+      addrs.forEach((addr) => {
+        csvData.push([addr, getRandomIntInclusive(1, 1000), timestamp]);
+      });
+    }
+    const csvExampleText = csvData.reduce(
+      (acc, value) => `${acc}${value[0]},${value[1]},${value[2]}\n`,
+      '',
+    );
+    logger('Generated Data', csvExampleText);
     const csvFile = new Blob([csvExampleText], { type: 'text/csv' });
     dispatch(setFile(new File([csvFile], 'sample.csv')));
     dispatch(setState(2));
